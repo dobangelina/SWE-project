c425efa598beb89c5b8d0f88b8cf48cfd0a695ea
dobangelina <196825112+dobangelina@users.noreply.github.com>
2026-02-10 13:52:29 +0000

    add runway class
    Signed-off-by: dobangelina <196825112+dobangelina@users.noreply.github.com>

---
+++ backend/runway.py
@@ -0,0 +1,26 @@
+import random
+
+class Runway:
+    def __init__(self, runway_id, runway_mode, runway_status):
+        self.id = runway_id
+        self.mode = runway_mode
+        self.status = runway_status
+        self.occupiedUntil = 0 #SimTime
+        self.currentAircraft = None
+        self.length = random.randint(2000,4000)
+        self.bearing = random.randint(1,36)
+
+    def isAvailable(self): #requires SimTime
+        pass
+
+    def canLand(self):
+        return self.occupiedUntil == None and ((self.Mode == "Mixed") or (self.Mode == "Landing"))
+
+    def canTakeOff(self):
+        return self.occupiedUntil == None and ((self.Mode == "Mixed") or (self.Mode == "TakeOff"))
+
+    def getBearingString(self):
+        if self.bearing <= 9:
+            return str("0" + str(self.bearing))
+        else:
+            return str(self.bearing)
