504ff6055bdf39cb9381c3e82d4ab58bb9d260c6
Vadim Pankov <88099200+Vadim3377@users.noreply.github.com>
2026-02-25 15:23:49 +0000

    Update runway.py

--- backend/runway.py
+++ backend/runway.py
@@ -3,7 +3,7 @@ import random
 class Runway:
     def __init__(self, runway_id, runway_mode, runway_status) -> None:
         self.id = runway_id
-        self.mode = runway_mode              # capability (DO NOT CHANGE)
+        self.mode = runway_mode              # capability 
         self.status = runway_status
         self.currentOperation = None         # "LANDING" or "TAKEOFF"
         self.occupiedUntil = 0
